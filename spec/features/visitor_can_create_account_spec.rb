80b4fef7f5d43f82c37318dd84c74e5fab4e33fb
Edilene <edilene_cruz@yahoo.com.br>
2017-05-20 17:26:48 -0600

    updating root path to accept both visitor and login user

--- spec/features/visitor_can_create_account_spec.rb
+++ spec/features/visitor_can_create_account_spec.rb
@@ -12,4 +12,29 @@ RSpec.feature "Visitor can create account" do
 
     expect(current_path).to eq(root_path)
   end
+
+  it "must have unique email" do
+    user1 = create(:user, email: "me@email.com")
+
+    visit '/users/new'
+
+    fill_in "Email", with: "me@email.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Create User"
+
+    expect(current_path).to_not eq(root_path)
+  end
+
+  it "when account is created user see Welcome" do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(:user)
+
+    visit root_path
+
+    
+
+    expect(page).to have_content("Welcome #{user.email}")
+  end
 end
