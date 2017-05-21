7f9a28e36b273a5c1b4bb03ced051a45068ffe28
Edilene <edilene_cruz@yahoo.com.br>
2017-05-20 19:25:34 -0600

    restaurant can be created, now need to be changed to ajax

--- spec/features/registered_user_can_create_restaurant_spec.rb
+++ spec/features/registered_user_can_create_restaurant_spec.rb
@@ -12,13 +12,24 @@ RSpec.feature "Adds restaurant" do
     expect(page).to have_link("Add a Restaurant")
   end
 
-  it "can toggle new restaurant form" do
+  it "can see new restaurant form" do
     user = create(:user)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit '/'
+
+    click_link("Add a Restaurant")
+
+    fill_in "Name", with: "Texas de Brazil"
+    fill_in "Cuisine", with: "Brazilian"
+    fill_in "City", with: "Denver"
+    fill_in "State", with: "CO"
+    fill_in "Zip", with: "80201"
+
+    click_button "Add restaurant"
     save_and_open_page
+    expect(page).to have_content("Texas de Brazil")
   end
 
 end
